329d48dd3b46be31bce397aac1b4f52404ef2c56
vrain ahuja <vahuja@ordergulp.com>
2020-05-28 22:23:54 -0700

    i've added a user state

--- gulp/Services/UserService.swift
+++ gulp/Services/UserService.swift
@@ -7,3 +7,55 @@
 //
 
 import Foundation
+import Firebase
+
+let userservice = _UserService()
+
+final class _UserService{
+    
+    var user = User()
+    var cart = [MenuItem]()
+    let auth = Auth.auth()
+    let db = Firestore.firestore()
+    var userListner : ListenerRegistration? = nil
+    var cartListner : ListenerRegistration? = nil
+    
+    var isGuest : Bool {
+        guard let isGuest = auth.currentUser else {
+            return true
+        }
+        return false
+    }
+    
+    func getUser() {
+        guard let currentUser = auth.currentUser else {return}
+        
+        let userRef = db.collection("users").document(currentUser.uid)
+        userListner = userRef.addSnapshotListener({ (snap, err) in
+            if let error = err {
+                print(error.localizedDescription)
+                return
+            }
+            guard let data = snap?.data() else {return}
+            self.user = User.init(data: data)
+        })
+        
+        let cartRef = userRef.collection("Cart")
+        cartListner = cartRef.addSnapshotListener({ (snap, err) in
+            if let error = err {
+                print(error.localizedDescription)
+                return
+            }
+        snap?.documents.forEach({ (document) in
+            let cartSelection = MenuItem.init(data: document.data())
+            self.cart.append(cartSelection)
+                
+            })
+            
+        })
+        
+    }
+   
+    
+}
+
