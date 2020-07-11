f50112c97f740324817c25135a41471e91a2fd2d
vrain ahuja <vahuja@ordergulp.com>
2020-07-11 16:44:55 -0700

    made UI changes to Sign Up page

--- gulp/Controller/signupPage.swift
+++ gulp/Controller/signupPage.swift
@@ -27,6 +27,9 @@ class signupPage: UIViewController {
     @IBOutlet weak var signupButton: UIButton!
     @IBOutlet weak var signinButton: UIButton!
     
+    //Label Outlets
+    @IBOutlet weak var phoneDisclamer: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configName()
@@ -35,21 +38,19 @@ class signupPage: UIViewController {
         let width = UIScreen.main.bounds.size.width
         let height = UIScreen.main.bounds.size.height
         
-        userEmail.frame = CGRect(x:  0, y: 0, width: width * 5/6, height: height * 1/10)
+        
         userEmail.autocapitalizationType = UITextAutocapitalizationType.none
         userEmail.placeholder = "Your email address"
-        userEmail.layer.cornerRadius = 5
-        userEmail.layer.borderWidth = 1
-        userEmail.layer.borderColor = UIColor.systemPink.cgColor
+        
         
         userPassword.placeholder = "Your account password!"
       
         
-        userPasswordConfirmation.frame = CGRect(x:  0, y: 200,  width: width * 5/6, height: height * 1/10)
+        
+        
+        phoneDisclamer.adjustsFontSizeToFitWidth = true
         userPasswordConfirmation.placeholder = "Password confirmation!"
-        userPasswordConfirmation.layer.cornerRadius = 5
-        userPasswordConfirmation.layer.borderWidth = 1
-        userPasswordConfirmation.layer.borderColor = UIColor.systemPink.cgColor
+        
 
 //        userName.layer.cornerRadius = 5
 //        userName.layer.borderWidth = 1
@@ -62,8 +63,26 @@ class signupPage: UIViewController {
         userPassword.delegate = self
         userPasswordConfirmation.delegate = self
         userName.delegate = self
+       
+        signupButton.layer.cornerRadius = 5
+        signupButton.layer.borderWidth = 1
+        signupButton.layer.borderColor = CG_Colors.darkPurple
+        signupButton.backgroundColor = UI_Colors.darkPurple
         
+        userName.attributedPlaceholder = NSAttributedString(string:"Your full name" , attributes: [NSAttributedString.Key.foregroundColor: UIColor.black])
+        userName.borderStyle = UITextField.BorderStyle.none
         
+        userEmail.attributedPlaceholder = NSAttributedString(string:"Your E-mail address", attributes: [NSAttributedString.Key.foregroundColor: UIColor.black])
+        userEmail.borderStyle = UITextField.BorderStyle.none
+        
+        userPassword.attributedPlaceholder = NSAttributedString(string:"E-mail address", attributes: [NSAttributedString.Key.foregroundColor: UIColor.black])
+        userPassword.borderStyle = UITextField.BorderStyle.none
+        
+        userPasswordConfirmation.attributedPlaceholder = NSAttributedString(string:"Confirm your password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.black])
+        userPasswordConfirmation.borderStyle = UITextField.BorderStyle.none
+        
+        userPhoneNumber.attributedPlaceholder = NSAttributedString(string:"Your phone number", attributes: [NSAttributedString.Key.foregroundColor: UIColor.black])
+        userPhoneNumber.borderStyle = UITextField.BorderStyle.none
        
             signupButton.setTitle("Sign Up", for: .normal)
             signupButton.showsTouchWhenHighlighted = true
