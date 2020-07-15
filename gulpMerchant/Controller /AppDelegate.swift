96fda64d50861f0017da25ada5c7c3bc23d447bd
vrain ahuja <vahuja@ordergulp.com>
2020-07-15 16:49:07 -0700

    starting to add stripe functionality

--- gulpMerchant/Controller /AppDelegate.swift
+++ gulpMerchant/Controller /AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import Stripe
 
 
 @UIApplicationMain
@@ -18,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-
+        Stripe.setDefaultPublishableKey("pk_test_LdIj43U3AT5gKjMChrv0cdFV00GsaCO40A")
         return true
     }
 
